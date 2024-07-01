ca2f583b06aae3d69602e05d3760712f35b5c719
VithuThevan <vithursan1003@gmail.com>
2024-07-01 14:40:04 +0530

    Did some changes

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -6,42 +6,43 @@
                 <legend>Review Payment Details</legend>
                 <div>
                     @foreach ($data["data"] as $name => $value)
-                        <div>
-                            @if ($name != "_token" 
-                              && $name != "profile_id"
-                              && $name != "transaction_uuid"
-                              && $name != "reference_number"
-                              && $name != "signed_field_names"
-                              && $name != "signed_date_time"
-                              && $name != "access_key"
-                              && $name != "locale"
-                              && $name != "transaction_type"
-                              && $name != "currency"
-                              )
-                                @if ($name == "donorName")
-                                    <span class="fieldName">Donor Name:</span>
-                                @elseif ($name == "donorEmail")
-                                    <span class="fieldName">Donor Email:</span>
-                                @elseif ($name == "donorPhone")
-                                    <span class="fieldName">Contact Name:</span>
-                                    @elseif ($name == "donorAddress")
-                                    <span class="fieldName">Address:</span>
-                                    @elseif ($name == "donationType")
-                                    <span class="fieldName">Donation Type:</span>
-                                    @elseif ($name == "donationPurpose")
-                                    <span class="fieldName">Donation Propose:</span>
-                                    @elseif ($name == "studentname")
-                                    <span class="fieldName">Name of the students:</span>
-                                    @elseif ($name == "admissionno")
-                                    <span class="fieldName">Admission No:</span>
-                                    @elseif ($name == "studentGrade")
-                                    <span class="fieldName">Student Grade:</span>
-                                    @elseif ($name == "amount")
-                                    <span class="fieldName">Donation Amount:</span>
-                                @endif
-                                <span class="fieldValue">{{ $value }}</span>
-                            @endif
-                        </div>
+                                        <div>
+                                            @if (
+                                                    $name != "_token"
+                                                    && $name != "profile_id"
+                                                    && $name != "transaction_uuid"
+                                                    && $name != "reference_number"
+                                                    && $name != "signed_field_names"
+                                                    && $name != "signed_date_time"
+                                                    && $name != "access_key"
+                                                    && $name != "locale"
+                                                    && $name != "transaction_type"
+                                                    && $name != "currency"
+                                                )
+                                                                    @if ($name == "donorName")
+                                                                        <span class="fieldName">Donor Name:</span>
+                                                                    @elseif ($name == "donorEmail")
+                                                                        <span class="fieldName">Donor Email:</span>
+                                                                    @elseif ($name == "donorPhone")
+                                                                        <span class="fieldName">Contact Name:</span>
+                                                                    @elseif ($name == "donorAddress")
+                                                                        <span class="fieldName">Address:</span>
+                                                                    @elseif ($name == "donationType")
+                                                                        <span class="fieldName">Donation Type:</span>
+                                                                    @elseif ($name == "donationPurpose")
+                                                                        <span class="fieldName">Donation Propose:</span>
+                                                                    @elseif ($name == "studentname")
+                                                                        <span class="fieldName">Name of the students:</span>
+                                                                    @elseif ($name == "admissionno")
+                                                                        <span class="fieldName">Admission No:</span>
+                                                                    @elseif ($name == "studentGrade")
+                                                                        <span class="fieldName">Student Grade:</span>
+                                                                    @elseif ($name == "amount")
+                                                                        <span class="fieldName">Donation Amount:</span>
+                                                                    @endif
+                                                                    <span class="fieldValue">{{ $value }}</span>
+                                            @endif
+                                        </div>
                     @endforeach
                 </div>
             </fieldset>
