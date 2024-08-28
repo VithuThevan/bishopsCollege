f4ab082b51d0ada49508d13003450dbd25709de7
Vithu <vithursan1003@gmail.com>
2024-08-28 09:19:19 +0530

    Did some changes

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -1,51 +1,44 @@
 <x-app-web-layout>
     <div class="container mt-5">
-        <h1>Review Payment Details</h1>
+        <h1 class="text-center">Review Payment Details</h1>
         <form id="payment_confirmation" action="https://testsecureacceptance.cybersource.com/pay" method="post">
             <fieldset id="confirmation">
-                <legend>Review Payment Details</legend>
-                <div>
+                <div class="row">
                     @foreach ($data["data"] as $name => $value)
-                                        <div>
-                                            @if (
-                                                    $name != "_token"
-                                                    && $name != "profile_id"
-                                                    && $name != "transaction_uuid"
-                                                    && $name != "reference_number"
-                                                    && $name != "signed_field_names"
-                                                    && $name != "signed_date_time"
-                                                    && $name != "access_key"
-                                                    && $name != "locale"
-                                                    && $name != "transaction_type"
-                                                    && $name != "currency"
-                                                )
-                                                                    @if ($name == "donorName")
-                                                                        <span class="fieldName">Donor Name:</span>
-                                                                    @elseif ($name == "donorEmail")
-                                                                        <span class="fieldName">Donor Email:</span>
-                                                                    @elseif ($name == "donorPhone")
-                                                                        <span class="fieldName">Contact Name:</span>
-                                                                    @elseif ($name == "donorAddress")
-                                                                        <span class="fieldName">Address:</span>
-                                                                    @elseif ($name == "donationType")
-                                                                        <span class="fieldName">Donation Type:</span>
-                                                                    @elseif ($name == "donationPurpose")
-                                                                        <span class="fieldName">Donation Propose:</span>
-                                                                    @elseif ($name == "studentname")
-                                                                        <span class="fieldName">Name of the students:</span>
-                                                                    @elseif ($name == "admissionno")
-                                                                        <span class="fieldName">Admission No:</span>
-                                                                    @elseif ($name == "studentGrade")
-                                                                        <span class="fieldName">Student Grade:</span>
-                                                                    @elseif ($name == "amount")
-                                                                        <span class="fieldName">Donation Amount:</span>
-                                                                    @endif
-                                                                    <span class="fieldValue">{{ $value }}</span>
-                                            @endif
-                                        </div>
+                        @if (!in_array($name, ["_token", "profile_id", "transaction_uuid", "reference_number", "signed_field_names", "signed_date_time", "access_key", "locale", "transaction_type", "currency"]))
+                            <div class="col-md-12 mb-2">
+                                <span class="fieldName font-weight-bold">
+                                    @switch($name)
+                                        @case("donorName") Donor Name: @break
+                                        @case("donorEmail") Donor Email: @break
+                                        @case("donorPhone") Contact Name: @break
+                                        @case("donorAddress") Address: @break
+                                        @case("donationType") Donation Type: @break
+                                        @case("donationPurpose") Donation Purpose: @break
+                                        @case("studentname") Name of the Students: @break
+                                        @case("admissionno") Admission No: @break
+                                        @case("studentGrade") Student Grade: @break
+                                        @case("amount") Donation Amount: @break
+                                        @case("bill_to_address_city") City: @break
+                                        @case("bill_to_address_line1") Address: @break
+                                        @case("bill_to_address_state") State: @break
+                                        @case("bill_to_address_country") Country: @break
+                                        @case("bill_to_email") Email: @break
+                                        @case("bill_to_address_postal_code") Postal Code: @break
+                                        @case("bill_to_forename") First Name: @break
+                                        @case("bill_to_phone") Phone: @break
+                                        @case("bill_to_surname") Last Name: @break
+                                        @case("card_type") Card Type: @break
+                                        @default {{ $name }}:
+                                    @endswitch
+                                </span>
+                                <span class="fieldValue">{{ $value }}</span>
+                            </div>
+                        @endif
                     @endforeach
                 </div>
             </fieldset>
+
             @foreach ($data as $name => $value)
                 @if (is_array($value))
                     @foreach ($value as $subKey => $subValue)
@@ -57,15 +50,20 @@
             @endforeach
             <input type="hidden" id="signature" name="signature" value="{{ $signature }}">
 
-            <input type="submit" id="submit" value="Confirm">
+            <div class="text-center mt-4">
+                <input type="submit" id="submit" class="btn btn-primary btn-lg" value="Confirm">
+            </div>
         </form>
     </div>
-
 </x-app-web-layout>
 
+
 <script>
+    const signature = '{{ $signature }}';
+    console.log("Signature:", signature);
+
     function sign(data) {
-        // This is a placeholder for the actual signature generation.
+        // Placeholder for the actual signature generation
         return 'dummy-signature';
     }
 </script>
